c318a1ce3e36bed4bb75c35c3dfdd3940f749722
Sayan Chakrabarti <me@sayanriju.co.cc>
2013-04-19 00:27:16 +0530

    begin222

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var irc = require('irc');
 var bot = new irc.Client('chat.freenode.net', process.env.OPENSHIFT_APP_NAME || 'ircbot', {
-    channels: ['#botzoo', '#botwar'],
+    channels: ['#testmybot'],
     port: 8001,
     debug: true
 });
