05d4474d689c6f7c2f4c743e86959becafbb5ed9
Sayan Chakrabarti <me@sayanriju.co.cc>
2013-04-29 01:24:03 +0530

    more factoids added; license change to DBAD PL

--- server.js
+++ server.js
@@ -2,22 +2,15 @@
  * Botokesto is an IRC bot written using node.js
  * *********************************************
  * Copyleft 2013 Sayan "Riju" Chakrabarti
- * *********************************************
+ * <s26c.sayan[at]gmail>
+ * *************************************************************
+ *  
+ * This program is free (as in Freedom) software; you can
+ * redistribute it and/or modify it under the terms of
+ * the DBAD Public License, as defined at:
+ * http://raw.github.com/philsturgeon/dbad/master/LICENSE-en.md
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ * *************************************************************
  */
 
 //process.exit();
@@ -97,7 +90,6 @@ function getResponse(factoid){
     case "justask" :
     case "question" :
     case "problem" :
-    case "question" :
         response="Please don't ask to ask a question, simply ask the question (all on ONE line and in the channel, so that others can read and follow it easily). If anyone knows the answer they will most likely reply. :-)";
         break;
     case "pastebin" :
@@ -137,6 +129,57 @@ function getResponse(factoid){
     case "cya" :
         response= S(factoid).capitalize().s+"! It was nice having you here, do come back soon! :=) ";
         break;
+        
+        
+    case "allcaps":
+    case "caps":
+    case "capslock":
+    case "shout":
+    case "shouting":
+        response="Please turn off your CAPSLOCK. Typing in all CAPS is akin to SHOUTING!";
+        break;
+    case "irc":
+    case "irc beginner":
+    case "irc guide":
+    case "irc howto":
+        response="For a nice IRC primer, please visit http://tldp.org/HOWTO/IRC/beginners.html";
+        break;
+    case "networketiquette":
+    case "networkettiquette":
+    case "etiquette":
+    case "ettiquette":
+    case "net etiquette":
+    case "net ettiquette":
+    case "network etiquette":
+    case "network ettiquette":
+    case "internet etiquette":
+    case "internet ettiquette":
+    case "netiquette":
+    case "nettiquette":
+    case "coc":
+    case "codeofconduct":
+    case "code of conduct":
+    case "conduct":
+    case "behave":
+    case "behavior":
+    case "behaviour":
+        response="To know how to maintain basic net etiquette, please visit http://www.networketiquette.net/ ";
+        break;  
+    case "n00b":
+    case "noob":
+    case "newbie":
+    case "beginner":
+    case "smart question":
+    case "how to ask":
+        response="If you are a newbie, perhaps this extensive treatise is a good place to start off: http://www.catb.org/esr/faqs/smart-questions.html";
+        break;
+    case "lnw":
+    case "linux is not windows":
+    case "linux vs windows":
+        response="Linux is Not Windows : please visit http://linux.oneandoneis2.org/LNW.htm to learn why.";
+        break;
+
+    
     case "who are you" :
     case "about you" :
     case "bot" :
@@ -157,20 +200,54 @@ function getResponse(factoid){
     case "information":
         response= "This is the official IRC channel for our GNU/Linux Users Group, Kolkata Chapter. Please visit our website http://ilug-cal.info for further information.";
         break;
+    case "website":
+    case "web":
+    case "web address":
+    case "url":
+    case "site":
+        response="Our website is at http://ilug-cal.info";
+        break;
     case "news" :
     case "latest news" :
     case "happening" :
-        response= "This is the official IRC channel for our GNU/Linux Users Group, Kolkata Chapter. Please visit our website http://ilug-cal.info for further information.";
+        response= "Please visit http://ilug-cal.info for information on latest news.";
         break;
     case "event" :
     case "events" :
     case "upcoming" :
     case "calendar" :
     case "event calendar" :
-        response= "This is the official IRC channel for our GNU/Linux Users Group, Kolkata Chapter. Please visit our website http://ilug-cal.info for further information.";
+        response= "Please visit http://ilug-cal.info for information on upcoming events.";
+        break;
+
+
+    case "windows":
+    case "windoze":
+    case "winblows":
+        response="Windows are frameworks of wood or metal containing a glass pane and are built into a wall or roof to admit light or air! ;=)";
+        break;
+    case "linux":
+    case "lunix":
+        response="Linux is JUST the kernel! May s/he who says anything to the contrary meet his/her FreeDoom!!! ;=)"
+        break;
+    case "gnu":
+    case "gnulinux":
+        response="That popular unix-like operating system must be called GNU/Linux! May s/he who says anything to the contrary meet his/her FreeDoom!!! ;=)"
+        break;
+    case "nitpick":
+    case "nitpicking":
+        response="Is this is what you're doing: http://en.wikipedia.org/wiki/Nitpicking_%28pastime%29 ? ;=)";
+        break;
+    case "rms":
+    case "stallman":
+    case "richard stallman":
+    case "richard mark stallman":
+        response="RMS is a living God! Visit http://stallman.org/ to bow & pray at his altar! ;=)"
         break;
         
-    
+    case "prayer":
+        response="Dear <your_favorite_deity_here>, Give me strength to understand and work with users who question my logic, the rules, netiquette, and common sense. Give me resilience to teach them the basics of Linux, GNU, FSF, and IRC. Allow me not to stray to nitpicking, argument, foul language, or leisurely op abuse. Deliver me my daily xkcd, User Friendly, LWN, and guard over my encrypted drives. Let it be so."
+        break;
     case "ping":
         response="pong!";
         break;
@@ -181,7 +258,8 @@ function getResponse(factoid){
     return response;
 }
 
-// Check for factoid commands
+// Check for factoid commands //
+
 bot.addListener("message",function(nick,channel,message){    // Check if starts with botname 
     if (S(message.toLowerCase()).startsWith(config.botName.toLowerCase())){
         var factoid=S(message.toLowerCase()).chompLeft(config.botName.toLowerCase()).stripPunctuation().trim().s;
