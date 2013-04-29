6245db65a6f76b44d6d6db2e6d9cb49e933f4e3c
Sayan Chakrabarti <me@sayanriju.co.cc>
2013-04-30 00:57:46 +0530

    fixed PM to bot; added fortune cookies

--- server.js
+++ server.js
@@ -92,194 +92,217 @@ function getResponse(factoid){
     var response='';
     switch (factoid.toLowerCase()){
 
-    case "ask" :
-    case "help" :
-    case "justask" :
-    case "question" :
-    case "problem" :
-        response="Please don't ask to ask a question, simply ask the question (all on ONE line and in the channel, so that others can read and follow it easily). If anyone knows the answer they will most likely reply. :-)";
-        break;
-    case "pastebin" :
-    case "paste" :
-    case "flood" :
-    case "flooding" :
-    case "pasting" :
-        response="For posting multi-line texts into the channel, please use http://pastebin.com and post the resulting link here.";
-        break;
-    case "language" :
-    case "profanity" :
-    case "nsfw" :
-    case "swear" :
-    case "curse" :
-    case "swearing" :
-    case "cursing" :
-        response="Please watch your language and topic to help keep this channel family-friendly, polite, and professional.";
-        break;    
-    case "hello" :
-    case "hi" :
-    case "howdy" :
-    case "hola" :
-    case "ahoy" :
-        response= S(factoid).capitalize().s+"! How are you doing? ";
-        break;
-    case "bye" :
-    case "goodbye" :
-    case "good bye" :
-    case "tata" :
-    case "ciao" :
-    case "see you":
-    case "hasta la vista":
-    case "au revoir":
-    case "c ya" :
-    case "cya" :
-        response= S(factoid).capitalize().s+"! It was nice having you here, do come back soon! :=) ";
-        break;
+        case "ask" :
+        case "help" :
+        case "justask" :
+        case "question" :
+        case "problem" :
+            response="Please don't ask to ask a question, simply ask the question (all on ONE line and in the channel, so that others can read and follow it easily). If anyone knows the answer they will most likely reply. :-)";
+            break;
+        case "pastebin" :
+        case "paste" :
+        case "flood" :
+        case "flooding" :
+        case "pasting" :
+            response="For posting multi-line texts into the channel, please use http://pastebin.com and post the resulting link here.";
+            break;
+        case "language" :
+        case "profanity" :
+        case "nsfw" :
+        case "swear" :
+        case "curse" :
+        case "swearing" :
+        case "cursing" :
+            response="Please watch your language and topic to help keep this channel family-friendly, polite, and professional.";
+            break;    
+        case "hello" :
+        case "hi" :
+        case "howdy" :
+        case "hola" :
+        case "ahoy" :
+            response= S(factoid).capitalize().s+"! How are you doing? ";
+            break;
+        case "bye" :
+        case "goodbye" :
+        case "good bye" :
+        case "tata" :
+        case "ciao" :
+        case "see you":
+        case "hasta la vista":
+        case "au revoir":
+        case "c ya" :
+        case "cya" :
+            response= S(factoid).capitalize().s+"! It was nice having you here, do come back soon! :=) ";
+            break;
+            
+            
+        case "allcaps":
+        case "caps":
+        case "capslock":
+        case "shout":
+        case "shouting":
+            response="Please turn off your CAPSLOCK. Typing in all CAPS is akin to SHOUTING!";
+            break;
+        case "irc":
+        case "irc beginner":
+        case "irc guide":
+        case "irc howto":
+            response="For a nice IRC primer, please visit http://tldp.org/HOWTO/IRC/beginners.html";
+            break;
+        case "networketiquette":
+        case "networkettiquette":
+        case "etiquette":
+        case "ettiquette":
+        case "net etiquette":
+        case "net ettiquette":
+        case "network etiquette":
+        case "network ettiquette":
+        case "internet etiquette":
+        case "internet ettiquette":
+        case "netiquette":
+        case "nettiquette":
+        case "coc":
+        case "codeofconduct":
+        case "code of conduct":
+        case "conduct":
+        case "behave":
+        case "behavior":
+        case "behaviour":
+            response="To know how to maintain basic net etiquette, please visit http://www.networketiquette.net/ ";
+            break;  
+        case "n00b":
+        case "noob":
+        case "newbie":
+        case "beginner":
+        case "smart question":
+        case "how to ask":
+            response="If you are a newbie, perhaps this extensive treatise is a good place to start off: http://www.catb.org/esr/faqs/smart-questions.html";
+            break;
+        case "lnw":
+        case "linux is not windows":
+        case "linux vs windows":
+            response="Linux is Not Windows : please visit http://linux.oneandoneis2.org/LNW.htm to learn why.";
+            break;
+
         
-        
-    case "allcaps":
-    case "caps":
-    case "capslock":
-    case "shout":
-    case "shouting":
-        response="Please turn off your CAPSLOCK. Typing in all CAPS is akin to SHOUTING!";
-        break;
-    case "irc":
-    case "irc beginner":
-    case "irc guide":
-    case "irc howto":
-        response="For a nice IRC primer, please visit http://tldp.org/HOWTO/IRC/beginners.html";
-        break;
-    case "networketiquette":
-    case "networkettiquette":
-    case "etiquette":
-    case "ettiquette":
-    case "net etiquette":
-    case "net ettiquette":
-    case "network etiquette":
-    case "network ettiquette":
-    case "internet etiquette":
-    case "internet ettiquette":
-    case "netiquette":
-    case "nettiquette":
-    case "coc":
-    case "codeofconduct":
-    case "code of conduct":
-    case "conduct":
-    case "behave":
-    case "behavior":
-    case "behaviour":
-        response="To know how to maintain basic net etiquette, please visit http://www.networketiquette.net/ ";
-        break;  
-    case "n00b":
-    case "noob":
-    case "newbie":
-    case "beginner":
-    case "smart question":
-    case "how to ask":
-        response="If you are a newbie, perhaps this extensive treatise is a good place to start off: http://www.catb.org/esr/faqs/smart-questions.html";
-        break;
-    case "lnw":
-    case "linux is not windows":
-    case "linux vs windows":
-        response="Linux is Not Windows : please visit http://linux.oneandoneis2.org/LNW.htm to learn why.";
-        break;
-
-    
-    case "who are you" :
-    case "who r u" :
-    case "about you" :
-    case "bot" :
-    case "yourself" :
-    case "you" :
-        response= "Hi, my name is "+config.realName+". I'm the resident bot of this channel, striving to make your stay here a pleasant one! :=)";
-        break;
-    //~ case "source code":
-    //~ case "your source":
-    //~ case "bot source":
-    //~ case "botsource":
-    //~ case "sourcecode":
-    //~ case "source":
-        //~ response="My source code may be found at";
-        //~ break;
-    //~ case "usage":
-    //~ case "use bot":
-    //~ case "bot usage":
-        //~ response=:"";
-        //~ break;
-    case "who we are" :
-    case "who are we" :
-    case "what is this" :
-    case "where am i" :
-    case "about":
-    case "about us" :
-    case "aboutus" :
-    case "channel" :
-    case "glugcalinfo" :
-    case "glugcal":
-    case "glug":
-    case "info":
-    case "information":
-        response= "This is the official IRC channel for our GNU/Linux Users Group, Kolkata Chapter. Please visit our website http://ilug-cal.info for further information.";
-        break;
-    case "website":
-    case "web":
-    case "web address":
-    case "url":
-    case "site":
-        response="Our website is at http://ilug-cal.info";
-        break;
-    case "news" :
-    case "latest news" :
-    case "happening" :
-    case "happenings" :
-        response= "Please visit http://ilug-cal.info for information on latest news regarding GLUG-Cal.";
-        break;
-    case "event" :
-    case "events" :
-    case "upcoming" :
-    case "calendar" :
-    case "event calendar" :
-        response= "Please visit http://ilug-cal.info for information on upcoming events at GLUG-Cal.";
-        break;
+        case "who are you" :
+        case "who r u" :
+        case "about you" :
+        case "bot" :
+        case "yourself" :
+        case "you" :
+            response= "Hi, my name is "+config.realName+". I'm the resident bot of this channel, striving to make your stay here a pleasant one! :=)";
+            break;
+        //~ case "source code":
+        //~ case "your source":
+        //~ case "bot source":
+        //~ case "botsource":
+        //~ case "sourcecode":
+        //~ case "source":
+            //~ response="My source code may be found at";
+            //~ break;
+        //~ case "usage":
+        //~ case "use bot":
+        //~ case "bot usage":
+            //~ response=:"";
+            //~ break;
+        case "who we are" :
+        case "who are we" :
+        case "what is this" :
+        case "where am i" :
+        case "about":
+        case "about us" :
+        case "aboutus" :
+        case "channel" :
+        case "glugcalinfo" :
+        case "glugcal":
+        case "glug":
+        case "info":
+        case "information":
+            response= "This is the official IRC channel for our GNU/Linux Users Group, Kolkata Chapter. Please visit our website http://ilug-cal.info for further information.";
+            break;
+        case "website":
+        case "web":
+        case "web address":
+        case "url":
+        case "site":
+            response="Our website is at http://ilug-cal.info";
+            break;
+        case "news" :
+        case "latest news" :
+        case "happening" :
+        case "happenings" :
+            response= "Please visit http://ilug-cal.info for information on latest news regarding GLUG-Cal.";
+            break;
+        case "event" :
+        case "events" :
+        case "upcoming" :
+        case "calendar" :
+        case "event calendar" :
+            response= "Please visit http://ilug-cal.info for information on upcoming events at GLUG-Cal.";
+            break;
 
 
-    case "windows":
-    case "windoze":
-    case "winblows":
-        response="Windows are frameworks of wood or metal containing a glass pane and are built into a wall or roof to admit light or air! ;=)";
-        break;
-    case "linux":
-    case "lunix":
-        response="Linux is JUST the kernel! May s/he who says anything to the contrary meet his/her FreeDoom!!! ;=)"
-        break;
-    case "gnu":
-    case "gnulinux":
-        response="That popular unix-like operating system must be called GNU/Linux! May s/he who says anything to the contrary meet his/her FreeDoom!!! ;=)"
-        break;
-    case "nitpick":
-    case "nitpicking":
-        response="Is this is what you're doing: http://en.wikipedia.org/wiki/Nitpicking_%28pastime%29 ? ;=)";
-        break;
-    case "rms":
-    case "stallman":
-    case "richard stallman":
-    case "richard mark stallman":
-        response="RMS is a living God! Visit http://stallman.org/ to bow & pray at his altar! ;=)"
-        break;
-        
-    case "prayer":
-    case "pray":
-        response="Dear <your_favorite_deity_here>, Give me strength to understand and work with users who question my logic, the rules, netiquette, and common sense. Give me resilience to teach them the basics of Linux, GNU, FSF, and IRC. Allow me not to stray to nitpicking, argument, foul language, or leisurely op abuse. Deliver me my daily xkcd, User Friendly, LWN, and guard over my encrypted drives. Let it be so."
-        break;
-    case "ping":
-        response="pong!";
-        break;
+        case "windows":
+        case "windoze":
+        case "winblows":
+            response="Windows are frameworks of wood or metal containing a glass pane and are built into a wall or roof to admit light or air! ;=)";
+            break;
+        case "linux":
+        case "lunix":
+            response="Linux is JUST the kernel! May s/he who says anything to the contrary meet his/her FreeDoom!!! ;=)"
+            break;
+        case "gnu":
+        case "gnulinux":
+            response="That popular unix-like operating system must be called GNU/Linux! May s/he who says anything to the contrary meet his/her FreeDoom!!! ;=)"
+            break;
+        case "nitpick":
+        case "nitpicking":
+            response="Is this is what you're doing: http://en.wikipedia.org/wiki/Nitpicking_%28pastime%29 ? ;=)";
+            break;
+        case "rms":
+        case "stallman":
+        case "richard stallman":
+        case "richard mark stallman":
+            response="RMS is a living God! Visit http://stallman.org/ to bow & pray at his altar! ;=)"
+            break;
 
-    default: response="Sorry, my responses are limited. You must ask the right question.";
+        case "fortune":
+        case "fortunecookie":
+        case "fortunecookies":
+        case "cookie":
+        case "have a cookie":
+        case "cookies":
+        case "factoid":
+        case "factoids":
+            var fs = require('fs');
+            var mersenne=require('mersenne');
+            var file =  './cookies.json';
+            var data=JSON.parse(fs.readFileSync(file, 'utf8'));
+            response=S(data[mersenne.rand(data.length)].text).trim().s;
+            break;
+            
+        case "prayer":
+        case "pray":
+            response="Dear <your_favorite_deity_here>, Give me strength to understand and work with users who question my logic, the rules, netiquette, and common sense. Give me resilience to teach them the basics of Linux, GNU, FSF, and IRC. Allow me not to stray to nitpicking, argument, foul language, or leisurely op abuse. Deliver me my daily xkcd, User Friendly, LWN, and guard over my encrypted drives. Let it be so."
+            break;
+        case "ping":
+            response="pong!";
+            break;
+        case "shut up":
+        case "shut up bot":
+        case "shutup":
+        case "shut your crap":
+            response="Hmm, you're certainly not the most well-mannered person I've met.";
+            break;
+
+        default: response="Sorry, my responses are limited. You must ask the right question.";
     }
 
     return response;
 }
+
+//var factoids=require("./factoids");
 
 // Check for factoid commands //
 
@@ -316,7 +339,10 @@ bot.addListener("message",function(nick,channel,message){    // Check if starts 
 
 });
 
-bot.addListener("pm",function(nick,channel,message){ // Handle PM to bot
-    var factoid=S(message.toLowerCase()).stripPunctuation().trim().s;  
-    bot.say(nick,getResponse(factoid));
+// Handle PM to bot
+bot.addListener("message",function(from_nick,to_nick,message){
+    if(to_nick.toLowerCase()==config.botName.toLowerCase()){ // It's a PM!  
+        var factoid=S(message.toLowerCase()).stripPunctuation().trim().s;  
+        bot.say(from_nick,getResponse(factoid));
+    }
 });
